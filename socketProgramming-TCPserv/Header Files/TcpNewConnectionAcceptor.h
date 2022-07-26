38fd3d6789f62d00e420bb01022b3829788d16f1
MatiasRepetto <repetto.matias3@gmail.com>
2022-07-26 12:57:10 -0300

    thread opps and acceptor WIP

--- socketProgramming-TCPserv/Header Files/TcpNewConnectionAcceptor.h
+++ socketProgramming-TCPserv/Header Files/TcpNewConnectionAcceptor.h
@@ -1,10 +1,15 @@
 #ifndef TCPNEWCONNECTIONACCEPTOR
 #define TCPNEWCONNECTIONACCEPTOR
 
+#include <thread>
+
 class TcpServerController;
+
 
 class TcpNewConnectionAcceptor{
     private:
+        int acepptr_verif;
+        std::thread* accept_new_con_thread;
     public:
         TcpServerController *tcp_svctrl;
         TcpNewConnectionAcceptor(TcpServerController *);
--- socketProgramming-TCPserv/src/TcpNewConnectionAcceptor.cpp
+++ socketProgramming-TCPserv/src/TcpNewConnectionAcceptor.cpp
@@ -1,8 +1,17 @@
 #include "../Header Files/TcpNewConnectionAcceptor.h"
 #include "../Header Files/TcpServerController.h"
+#include <iostream>
+#include <Winsock2.h>
+#include <memory.h>
+
 
 TcpNewConnectionAcceptor::TcpNewConnectionAcceptor(TcpServerController *tcpSC){
     this->tcp_svctrl = tcpSC;
+    this->acepptr_verif = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(this->acepptr_verif < 0){
+        std::cout << "Error al conectar, codigo: " << this->acepptr_verif; 
+    }
+    this->accept_new_con_thread = (std::thread*)calloc(1, sizeof(std::thread));
 }
 
 TcpNewConnectionAcceptor::~TcpNewConnectionAcceptor(){
